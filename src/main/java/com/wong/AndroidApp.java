67e1d3bd8d664e77767f152f92395c8f26a96864
wongGNOW <923084107@qq.com>
2021-11-26 10:10:52 +0800

    a springboot server for android app.I think it would provide the server of mongodb querying and mysql operation and some 3 party server like the OCR server from Baidu,the translation server of YouDao.                                              -- 2021/11/26

---
+++ src/main/java/com/wong/AndroidApp.java
@@ -0,0 +1,11 @@
+package com.wong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidApp.class);
+    }
+}
